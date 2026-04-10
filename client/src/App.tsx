e02297aebf871d81694ab71009ad6b688e6655ba
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-04-10 23:39:18 +0300

    Setup Tailwind CSS
    Installed and initialized Tailwind CSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@
 function App() {
   return (
     <>
+      <div className="mt-4 ml-4 text-2xl font-bold">Welcome to JobFlow AI!</div>
+      <div className="mt-4 ml-4 text-md italic">JobFlow AI is an AI-powered job application engine that automates the tedious parts of the job hunt.</div>
     </>
   )
 }
