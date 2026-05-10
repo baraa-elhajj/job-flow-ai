5caac82307144967be3049f187432df64f707be1
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-05-10 23:18:33 +0300

    Add app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,20 @@
-
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import JobsList from "./pages/JobsList";
+// import JobDetail from "./pages/JobDetails";
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/jobs" element={<JobsList />} />
+        {/* <Route path="/jobs/:id" element={<JobDetails />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
