689cd3c05236ddde46c664c99240b3d3dfd1f0bc
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-05-10 22:27:17 +0300

    Update App.tsx
    include home.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
 
+import Home from './pages/Home'
+
 function App() {
-  return (
-    <>
-      <div className="mt-4 ml-4 text-2xl font-bold">Welcome to JobFlow AI!</div>
-      <div className="mt-4 ml-4 text-md italic">JobFlow AI is an AI-powered job application engine that automates the tedious parts of the job hunt.</div>
-    </>
-  )
+  return <Home />
 }
 
 export default App
