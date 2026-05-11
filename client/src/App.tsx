86d60a2686900031d88899af9d57893c5688500e
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-05-11 06:01:16 +0300

    Updated routes + added NotFound route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import JobsList from "./pages/JobsList";
-// import JobDetail from "./pages/JobDetails";
+// import JobDetails from "./pages/JobDetails";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/jobs" element={<JobsList />} />
+          {/* <Route path="/jobs/:id" element={<JobDetails />} /> */}
+        </Route>
         <Route path="/login" element={<Login />} />
-        <Route path="/jobs" element={<JobsList />} />
-        {/* <Route path="/jobs/:id" element={<JobDetails />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
