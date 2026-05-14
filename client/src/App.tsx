4b329882c470371a762f274fbd73534da9069641
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-05-14 14:20:31 +0300

    Update routes and home buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import JobsList from "./pages/JobsList";
-import LinkedInJobsList from "./pages/LinkedInJobsList";
 import JobDetails from "./pages/JobDetails";
-import LinkedInJobDetails from "./pages/LinkedInJobDetails";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 
@@ -14,10 +12,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/jobs" element={<JobsList />} />
-          <Route path="/jobs/linkedin" element={<LinkedInJobsList />} />
+          <Route path="/jobs" element={<JobsList source="hn" />} />
+          <Route
+            path="/jobs/linkedin"
+            element={<JobsList source="linkedin" />}
+          />
           <Route path="/jobs/:id" element={<JobDetails />} />
-          <Route path="/jobs/linkedin/:id" element={<LinkedInJobDetails />} />
+          <Route path="/jobs/linkedin/:id" element={<JobDetails />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
