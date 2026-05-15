28f29abcafdd042d1296b9468cb2bd8f9aefbc41
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-05-16 01:07:33 +0300

    Disable TS linkedIn scraper job

--- server/services/jobScraperScheduler.ts
+++ server/services/jobScraperScheduler.ts
@@ -3,26 +3,32 @@ import { scrapeAndStoreLinkedInJobs } from "./linkedinScraper.js";
 import { scrapeAndStoreHNHiringJobs } from "./hnhiringScraper.js";
 
 export function startScraperScheduler() {
+  const linkedInScrapeAllowed = false; // set to true (or remove) when TS LinkedIn Scraper is fixed.
   console.log("Starting job scraper scheduler...");
 
-  // LinkedIn scrape at 12:05 & 00:05 daily
+  // LinkedIn scraper at 12:05 & 00:05 daily
   cron.schedule("5 0,12 * * *", async () => {
-    console.log("Starting LinkedIn scrape...");
+    console.log("Starting LinkedIn scraper...");
+
+    if (!linkedInScrapeAllowed) {
+      console.warn("Skipping LinkedIn scraper until it is fixed");
+      return;
+    }
 
     try {
       await scrapeAndStoreLinkedInJobs(
         "https://www.linkedin.com/jobs/search/?keywords=software%20engineer&location=United%20States&f_TP=1%2C2",
         25,
       );
-      console.log("LinkedIn scrape completed successfully");
+      console.log("LinkedIn scraper completed successfully");
     } catch (error) {
-      console.error("LinkedIn scrape failed:", error);
+      console.error("LinkedIn scraper failed:", error);
     }
   });
 
-  // HN Hiring scrape every 2 hours on the first 5 days of the month, then every 12 hours on the rest of the month
+  // HN Hiring scraper every 2 hours on the first 5 days of the month, then every 12 hours on the rest of the month
   cron.schedule("0 */2 * * *", async () => {
-    console.log("Starting HN Hiring scrape...");
+    console.log("Starting HN Hiring scraper...");
 
     try {
       const now = new Date();
@@ -35,12 +41,12 @@ export function startScraperScheduler() {
 
       if (shouldScrape(hour, day)) {
         await scrapeAndStoreHNHiringJobs(month, year);
-        console.log("HN Hiring scrape completed successfully");
+        console.log("HN Hiring scraper completed successfully");
       } else {
-        console.log("Skipping HN Hiring scrape");
+        console.log("Skipping HN Hiring scraper in off hours");
       }
     } catch (error) {
-      console.error("HN Hiring scrape failed:", error);
+      console.error("HN Hiring scraper failed:", error);
     }
   });
 }
