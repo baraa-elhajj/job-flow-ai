b96d7720a7b6529cbe6b267e4d4467bdf4ebd92b
Baraa El Hajj <baraa.w.hajj@gmail.com>
2026-06-07 18:29:32 +0300

    Update jobsRoutes.ts
    Remove redundant api route

--- server/routes/jobsRoutes.ts
+++ server/routes/jobsRoutes.ts
@@ -1,12 +1,8 @@
 import express from "express";
-import {
-    fetchJobs,
-    fetchParsedJobById,
-} from "../controllers/jobsController.js";
+import { fetchJobs } from "../controllers/jobsController.js";
 
 const router = express.Router();
 
 router.get("/", fetchJobs);
-router.get("/parsed/:id", fetchParsedJobById);
 
 export default router;
